4760d331ffec803da74bacce515299d61b30b5f0
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2023-03-03 12:15:24 +0100

    popularMovies 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgIf, NgFor } from '@angular/common';
+
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, NgIf],
+  imports: [AsyncPipe, NgIf, NgFor],
   selector: 'cours-web-mobile-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   joke$ = this.appService.getJoke();
-
-  constructor(private readonly appService: AppService) {}
+  popularMovies$ = this.appService.getPopularMovies();
+  
+  constructor(private readonly appService: AppService) {
+    console.log(this.appService.getPopularMovies(), "ccc", this.appService.getJoke())
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -8,13 +8,32 @@ export interface Joke {
   delivery: string;
 }
 
+export interface Result {
+  id: number;
+  title: string;
+  overview : string;
+  popularity : number;
+  poster_path : string;
+}
+
+export interface Results {
+  results: Array<Result>;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AppService {
+  private readonly apiKey = '4789d4caefcebacc74ede26d39fe8048';
+  private readonly apiUrl = 'https://api.themoviedb.org/3/movie/popular';
+
   constructor(private httpClient: HttpClient) {}
 
   public getJoke(): Observable<Joke> {
     return this.httpClient.get<Joke>(
       'https://v2.jokeapi.dev/joke/Programming?type=twopart&safe-mode'
     );
+  }
+
+  public getPopularMovies(): Observable<Results> {
+    return this.httpClient.get<Results>(`${this.apiUrl}?api_key=${this.apiKey}&language=en-US&page=1`);
   }
 }
