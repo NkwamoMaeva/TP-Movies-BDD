f0c1ae8152a8509e7bb16dd2b77e8f5d4c69bb8d
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2023-04-07 01:34:26 +0200

    masque user page

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AngularFireAuth, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.auth.authState.pipe(
+      take(1),
+      map((user) => !!user),
+      tap((loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigate(['/login']);
+        }
+      })
+    );
+  }
+}
